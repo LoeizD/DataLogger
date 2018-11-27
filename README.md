85aaeef07f17afa1849d3b3d29b6fd344d553c29
Corentin Derbré <poubelle.co@free.fr>
2018-11-27 12:53:24 +0800

    nano reddit logger

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DataLogger
+
+## How to use this software
+
+1. run `npm install` to install the node modules
+2. 
+
+## NPM Depndencies
+- `node-fetch` for getting the data
---
+++ app.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch')
+var fs = require('fs')
+
+const hourInterval = 24
+const endpoint = "https://www.reddit.com/r/nanocurrency"
+
+function logData() {
+    fetch(endpoint)
+    .then(res => res.text())
+    .then(body => {
+        const arr = body.split("</")
+        arr.forEach((e, i) => {
+            if (e.match(">Subscribers")) {
+                // console.log(e + " -- INDEX -- " + i)
+                const index = i - 1
+                const pArr = arr[index].split(">")
+                const numUsersK = pArr[pArr.length - 1]
+                const numUsersnoK = parseFloat(numUsersK) * 1000
+                
+                fs.appendFileSync('nano-data.csv', `${Date.now()},${numUsersnoK}\n`)
+            }
+        });
+    });
+}
+
+setInterval(logData, hourInterval * 3600)
